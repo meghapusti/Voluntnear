972805c91f29a2ef56d9bd11d91fd3415d59bb6d
glendakoh <glendakoh88@gmail.com>
2024-04-09 16:20:30 +0800

    edit request and user class

--- app/src/main/java/com/example/voluntnear/classes/Request.java
+++ app/src/main/java/com/example/voluntnear/classes/Request.java
@@ -1,5 +1,6 @@
 package com.example.voluntnear.classes;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.Exclude;
 
 public class Request {
@@ -7,15 +8,25 @@ public class Request {
     private String userId;
     private String requestType;
 
+    private String date;
+    private String time;
+    private LatLng initLoc;
+    private LatLng destLoc;
+
+
     // Default constructor (required by Firebase)
     public Request() {
         // Default constructor required for calls to DataSnapshot.getValue(Request.class)
     }
 
-    public Request(String requestId, String userId, String requestType) {
+    public Request(String requestId, String userId, String requestType,String date,String time,LatLng initLoc,LatLng destLoc) {
         this.requestId = requestId;
         this.userId = userId;
         this.requestType = requestType;
+        this.date=date;
+        this.time=time;
+        this.initLoc=initLoc;
+        this.destLoc=destLoc;
     }
 
     public String getRequestId() {
@@ -41,6 +52,15 @@ public class Request {
     public void setRequestType(String requestType) {
         this.requestType = requestType;
     }
+
+    public String getDate(){return date;}
+    public void setDate(String date){this.date=date;}
+    public String getTime(){return time;}
+    public void setTime(String time){this.time=time;}
+    public LatLng getInitLoc(){return initLoc;}
+    public void setInitLoc(LatLng initLoc){this.initLoc=initLoc;}
+    public LatLng getDestLoc(){return destLoc;}
+    public void setDestLoc(LatLng destLoc){this.destLoc=destLoc;}
 
     // Exclude the requestId from being stored in the database (since it's already the key)
     @Exclude
--- app/src/main/java/com/example/voluntnear/classes/User.java
+++ app/src/main/java/com/example/voluntnear/classes/User.java
@@ -1,7 +1,14 @@
 package com.example.voluntnear.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String email, name, password, phno, role;
+    private List<Request> accepted_list;
+    private List<Request> pending_list;
+
+
 
     public User(String email, String name, String password, String phno, String role) {
         this.email = email;
@@ -9,6 +16,8 @@ public class User {
         this.password = password;
         this.phno = phno;
         this.role = role;
+        this.accepted_list=new ArrayList<>();
+        this.pending_list=new ArrayList<>();
     }
 
     public String getName() {
@@ -34,5 +43,18 @@ public class User {
     public void setPhno(String phno) {
         this.phno = phno;
     }
+
+    public List<Request> getAccepted_list(){return accepted_list;}
+    public List<Request> getPending_list(){return pending_list;}
+
+    public void add_accepted(Request new_req){
+        accepted_list.add(new_req);
+    }
+    public void remove_accepted(Request req){accepted_list.remove(req);}
+    public void add_pending(Request new_req){
+        pending_list.add(new_req);
+
+    }
+    public void remove_pending(Request req){pending_list.remove(req);}
 
 }
